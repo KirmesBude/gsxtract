d3ed0c9bdf08db116eb77e6bda03cb191524f571
KirmesBude <kirmesbude@gmail.com>
2021-03-10 22:04:59 +0100

    Implement CLI argument parsing
    Fixes #1

--- gsxtract-cli/src/main.rs
+++ gsxtract-cli/src/main.rs
@@ -1,14 +1,71 @@
 use gsxtract::*;
 use std::{
-    fs::{self, File},
+    fs::File,
     io,
 };
 use texture_packer::{texture::Texture, exporter::ImageExporter};
+use clap::{Arg, App};
 
 pub fn main() -> Result<(), io::Error> {
-    fs::create_dir_all("output").unwrap();
+    /* basic app information */
+    let app = App::new("gsxtract")
+        .version("0.1")
+        .about("Extracts all kinds of data from the ROMs of both GBA Golden Sun titles")
+        .author("KirmesBude");
 
-    let rom = GSRom::new("references/Golden Sun - The Lost Age.gba")?;
+    /* define the path command line option */
+    let path_option = Arg::with_name("path")
+        .last(true)
+        .takes_value(true)
+        .help("Path to a Golden Sun ROM")
+        .required(true);
+    let app = app.arg(path_option);
+
+    /* define the extract command line option */
+    let output_option = Arg::with_name("output")
+        .long("output")
+        .short("o")
+        .takes_value(true)
+        .help("Output path")
+        .required(true);
+    let app = app.arg(output_option);
+
+    /* define the verbosity command line option */
+    let verbosity_option = Arg::with_name("verbosity")
+        .short("v")
+        .multiple(true)
+        .help("Sets the level of verbosity")
+        .required(false);
+    let app = app.arg(verbosity_option);
+
+    /* extract matches */
+    let matches = app.get_matches();
+
+    /* extract the actual output */
+    let output = matches.value_of("output")
+        .expect("This can't be None, we said it was required");
+
+    /* extract the actual path */
+    let path = matches.value_of("path")
+        .expect("This can't be None, we said it was required");
+
+    /* extract the actual verbosity */
+    /* TODO: link to log? */
+    match matches.occurrences_of("v") {
+        0 => println!("No verbose info"),
+        1 => println!("Some verbose info"),
+        2 => println!("Tons of verbose info"),
+        3 | _ => println!("Don't be crazy"),
+    }
+
+    let rom = GSRom::new(path)?;
+
+    extract_sprites(&rom, output);
+
+    Ok(())
+}
+
+fn extract_sprites(rom: &GSRom, output: &str) {
     let test = rom.decompress_sprites();
 
     let packer_conf = texture_packer::TexturePackerConfig {
@@ -24,7 +81,7 @@ pub fn main() -> Result<(), io::Error> {
     for atlas in test {
         let mut packer = texture_packer::TexturePacker::new_skyline(packer_conf);
         let name = format!("{}.png", atlas.identifier());
-        let path = format!("output/{}", name);
+        let path = format!("{}/{}", output, name);
 
         for (i, sprite) in atlas.get_sprites().iter().enumerate() {
             let buffer = sprite.get_buffer();
@@ -44,5 +101,4 @@ pub fn main() -> Result<(), io::Error> {
             .write_to(&mut file, image::ImageFormat::Png)
             .unwrap();
     }
-    Ok(())
 }
